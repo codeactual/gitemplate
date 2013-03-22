2bd8da45b5e2eccd872bd6c70763fbb4440487ee
David Smith <codeactual@gmail.com>
2013-03-21 22:40:55 -0700

    Add test plan

--- test.js
+++ test.js
@@ -4,7 +4,63 @@ var should = chai.should();
 chai.Assertion.includeStack = true;
 
 describe('compake', function() {
-  it('should', function(testDone) {
-    testDone();
+  describe('Compake', function() {
+    it('should add license from local file', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add license from Github', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add TravisCI config', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add test.js', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add index.js', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add test.js', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add component.json', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add package.json', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add Gruntfile.json', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add .gitignore', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should add README', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should init git repo', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+  });
+
+  describe('CLI', function() {
+    it('should not init git repo by default', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
+
+    it('should display full destination path', function(testDone) {
+      console.log('\x1B[33mINCOMPLETE'); testDone(); // TODO
+    });
   });
 });
